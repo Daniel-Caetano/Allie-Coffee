70836c22f781aed0c390c5269428b91af645e0b1
Daniel Caetano de Souza Ferreira <danielcaetano.sf@gmail.com>
2022-03-15 16:20:25 -0300

    Adicionando Projetos genéricos do console, API e Biblioteca na Solution AC.Back

---
+++ AC.Back/AC.Back/AC.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AC.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AC.Back/AC.Back/AC.Servicos/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AC.Servicos
+{
+    public class Class1
+    {
+    }
+}
