dc86288a34ff4e1af309a15b5127591cef17596e
Paulo Ricardo <pauloramorims@gmail.com>
2022-03-18 11:07:29 -0300

    Adicionado e testado as classes Cardapio, CardapioMensal e Produto em Negócio

---
+++ AC.Back/AC.Servicos/Negocio/Cardapio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AC.Servicos.Negocio
+{
+    public class Cardapio : Entity
+    {
+        public DateTime Data { get; set; }
+        public Produto Produto { get; set; }
+        public Cardapio()
+        {
+            Data = DateTime.Now;
+            Produto = new Produto();
+        }
+
+        public Cardapio(Produto produto)
+        {
+            Produto = produto;
+        }
+
+        public override string ToString()
+        {
+            return $"Produto: {Produto} Data: {Data}";
+        }
+
+    }
+}
