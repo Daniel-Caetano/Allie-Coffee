15ab88834a021e901d5fdeef109aeb53555ecabb
Paulo Ricardo <pauloramorims@gmail.com>
2022-03-18 10:13:28 -0300

    script db

---
+++ AC.Back/AC.Servicos/Script/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AC.Servicos.Script
+{
+    public  class AppDbContext
+    {
+    }
+}
